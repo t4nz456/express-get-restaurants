df6f71ba6d7dae57b4bfea899efe40a1beefdce8
t4nz456 <t4nz1234@gmail.com>
2023-05-12 02:08:46 +0100

    part2

--- src/app.js
+++ src/app.js
@@ -5,10 +5,38 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get("/restaurants", async (req, res) => {
     const restaurants = await Restaurant.findAll();
     res.json(restaurants);
 })
+
+app.get("/restaurants/:id", async (req, res) => {
+    const data = req.params.id;
+    const find = await Restaurant.findByPk(data);
+    res.json(find);
+})
+
+app.post("/restaurants", async (req, res) => {
+    const data =  await Restaurant.create(req.body);
+    res.json(data);
+})
+
+app.put("/restaurants/:id", async (req, res) => {
+    const find = await Restaurant.update(req.body, {where: {id: req.params.id}});
+    res.json(find);
+})
+
+app.delete("/restaurants/:id", async (req, res) => {
+    const data = await Restaurant.destroy({where: {id: req.params.id}});
+    res.json(data);
+})
+
+
+
+
 
 
 module.exports = app;
